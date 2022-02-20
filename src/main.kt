c18cccc41241c2fabc36c80c4c91501354e0f1ef
Mikita Valatovich <mr.volotovich@gmail.com>
2022-02-20 22:54:13 +0100

    Feature 1: test gpg signature

--- src/main.kt
+++ src/main.kt
@@ -9,6 +9,7 @@ const val testVector5 = "bc1pw508d6qejxtdg4y5r3zarvary0c5xw7kw508d6qejxtdg4y5r3z
 fun main(){
     print("PA193 course project start.")
     test()
+    print("End")
 }
 
 fun test(){
