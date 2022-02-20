69184727144a88fe5bc5a8033415f3250f739ad3
Mikita Valatovich <mr.volotovich@gmail.com>
2022-02-20 16:31:21 +0100

    travis yml file

--- src/main.kt
+++ src/main.kt
@@ -1,10 +1,18 @@
 
 // From https://en.bitcoin.it/wiki/Bech32#:~:text=Bech32%20is%20a%20segwit%20address,is%20the%20preferred%20address%20scheme.
-val testVector1 = "4ef47f6eb681d5d9fa2f7e16336cd629303c635e8da51e425b76088be9c8744c"
-val testVector2 = "514a33f1d46179b89e1fea7bbb07b682ab14083a276979f91038369d1a8d689b"
-val testVector3 = "bc1qar0srrr7xfkvy5l643lydnw9re59gtzzwf5mdq"
-val testVector4 = "bc1qc7slrfxkknqcq2jevvvkdgvrt8080852dfjewde450xdlk4ugp7szw5tk9"
+const val testVector1 = "4ef47f6eb681d5d9fa2f7e16336cd629303c635e8da51e425b76088be9c8744c"
+const val testVector2 = "514a33f1d46179b89e1fea7bbb07b682ab14083a276979f91038369d1a8d689b"
+const val testVector3 = "bc1qar0srrr7xfkvy5l643lydnw9re59gtzzwf5mdq"
+const val testVector4 = "bc1qc7slrfxkknqcq2jevvvkdgvrt8080852dfjewde450xdlk4ugp7szw5tk9"
 
 fun main(){
     print("PA193 course project start.")
+    test()
+}
+
+fun test(){
+    print(testVector1)
+    print(testVector2)
+    print(testVector3)
+    print(testVector4)
 }
