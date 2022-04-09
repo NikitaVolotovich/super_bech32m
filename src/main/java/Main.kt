fc2b7be21147c7f08620b3f81de9aeef32fc4b7f
Illia Kostenko <kosten.il2000@gmail.com>
2022-04-09 19:32:21 +0200

    added test vectors

--- src/main/java/Main.kt
+++ src/main/java/Main.kt
@@ -1,4 +1,12 @@
+//Valid bech32 vector
 const val testVector5 = "bc1pw508d6qejxtdg4y5r3zarvary0c5xw7kw508d6qejxtdg4y5r3zarvary0c5xw7k7grplx"
+const val bech32Vector2 = "bc1p0xlxvlhemja6c4dqv22uapctqupfhlxm9h8z3k2e72q4k9hcz7vqh2y7hd"
+const val bech32Vector3 = "tb1z0xlxvlhemja6c4dqv22uapctqupfhlxm9h8z3k2e72q4k9hcz7vqglt7rf"
+
+//Valid bech32m vector
+const val bech32mVector1 = "tb1q0xlxvlhemja6c4dqv22uapctqupfhlxm9h8z3k2e72q4k9hcz7vq24jc47"
+const val bech32mVector2 = "an83characterlonghumanreadablepartthatcontainsthetheexcludedcharactersbioandnumber11sg7hg6"
+const val bech32mVector3 = "split1checkupstagehandshakeupstreamerranterredcaperredlc445v"
 
 fun main(args: Array<String>) {
     Logger.clean()
